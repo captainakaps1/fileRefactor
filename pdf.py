fa75da73c785f06df316d9955c3fd23db65824c6
David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>
2020-05-15 11:23:56 +0000

    pdf code
    Signed-off-by: David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>

--- pdf.py
+++ pdf.py
@@ -30,8 +30,8 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
-i = ''
-j = 0
+word_list = ''
+word = 0
 with open("A.txt", 'r') as data_dict:
     file_text = data_dict.read().splitlines()
     print(file_text)
@@ -40,50 +40,52 @@ with open("A.txt", 'r') as data_dict:
             print(file + " opened")
             pdfText = convert_pdf_to_txt("./1/" + file)
             pdfText = pdfText.split('.')
-            filename = file.replace('.pdf', '')
-            r_count: int = 0
-            while r_count < pdfText.__len__():
-                pdfText[r_count] = pdfText[r_count].replace('\n', ' ')
+            file_name = file.replace('.pdf', '')
+            line_index: int = 0
+            while line_index < pdfText.__len__():
+                pdfText[line_index] = pdfText[line_index].replace('\n', ' ')
                 v = 0
                 while v < file_text.__len__():
                     line = file_text[v].split('|')
-                    for i in line:
+                    for word_list in line:
                         test = True
-                        if i == line[-1]:
+                        if word_list == line[-1]:
                             break
-                        i = i.split(',')
-                        while j < i.__len__():
+                        word_list = word_list.split(',')
+                        for word in word_list:
                             try:
-                                pdfText[r_count].upper().index(i[j].upper())
-                                f = pdfText[r_count]
-                                j += 1
+                                pdfText[line_index].upper().index(word.upper())
+                                string_test = pdfText[line_index]
                                 test = False
-                                with open("./Community_" + filename + ".txt", 'a+') as out_file:
-                                    # print("Writing to " + word + ".txt has begun")
-                                    out_file.write('\n\n')
-                                    out_file.write(
-                                        '--------------------------------------------------------------------------'
-                                        '-------------------------------------------------- '
-                                        + line[-1] +
-                                        '--------------------------------------------------------------------------'
-                                        '-------------------------------------------------- \n')
-                                    # print((pdfText[r_count].encode('ascii', 'ignore')).decode('utf-8') + '\n')
-                                    out_file.write(
-                                        (pdfText[r_count].encode('ascii', 'ignore')).decode('utf-8') + '\n')
-                                    # print("Writing to " + word + ".txt has executed")
-                                    out_file.write('\n\n')
-                                break
+                                try:
+                                    line[-2].index(word)
+                                    with open("./Community_" + file_name + ".txt", 'a+') as out_file:
+                                        # print("Writing to " + word + ".txt has begun")
+                                        out_file.write('\n\n')
+                                        out_file.write(
+                                            '--------------------------------------------------------------------------'
+                                            '-------------------------------------------------- '
+                                            + line[-1] +
+                                            '--------------------------------------------------------------------------'
+                                            '-------------------------------------------------- \n')
+                                        # print((pdfText[line_index].encode('ascii', 'ignore')).decode('utf-8') + '\n')
+                                        out_file.write(
+                                            (pdfText[line_index].encode('ascii', 'ignore')).decode('utf-8') + '\n')
+                                        # print("Writing to " + word + ".txt has executed")
+                                        out_file.write('\n\n')
+                                    break
+                                except ValueError:
+                                    pass
                             except ValueError:
-                                j += 1
                                 pass
-                                # print("f")
+                                # print("string_test")
                         if test:
                             break
                         # except ValueError:
                         #     continue
                         #     # print("g")
                     v += 1
-                r_count = r_count + 1
+                line_index = line_index + 1
         except ValueError:
             print(file + "failed")
             continue
--- renaming.py
+++ renaming.py
@@ -2,7 +2,8 @@ import glob
 import os
 import xlrd
 import shutil
-import zipfile
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 import re
 
 # create folder new_folder
@@ -19,8 +20,8 @@ import re
 #   highlight line in excel file
 # else:
 #   move to next line
-#
 
+ttt = []
 count = 1
 hold = ''
 try:
@@ -40,65 +41,77 @@ for p in os.listdir('.'):
         for i in range(sheet.nrows):
             if i == 0:
                 continue
-            elif hold == sheet.cell_value(i, 1):
-                continue
-
-            hold = sheet.cell_value(i, 1)
 
             folder_name_outer = sheet.cell_value(i, 3)
             folder_name_inner = sheet.cell_value(i, 1)
 
             if p.capitalize() == folder_name_outer.capitalize():
-
                 try:
                     lsdir_o = os.listdir('./' + p)
-                    h = 0
-                    while h < lsdir_o.__len__():
-                        lsdir_o[h] = lsdir_o[h].upper()
-                        h = h + 1
                     try:
-                        #print(lsdir_o)
-
-                        if lsdir_o[0].istitle():
-                            folder_name_inner =  folder_name_inner.capitalize()
-                        elif lsdir_o[0].isupper():
-                            folder_name_inner = folder_name_inner.upper()
-                        elif lsdir_o[0].islower():
-                            folder_name_inner = folder_name_inner.lower()
-
-
+                        try:
+                            lsdir_o.index(folder_name_inner)
+                        except:
+                            if lsdir_o[0].istitle():
+                                folder_name_inner = folder_name_inner.capitalize()
+                            elif lsdir_o[0].isupper():
+                                folder_name_inner = folder_name_inner.upper()
+                            elif lsdir_o[0].islower():
+                                folder_name_inner = folder_name_inner.lower()
                         basepath = './' + folder_name_outer + '/' + folder_name_inner
-                        print(os.listdir(basepath))
+                        try:
+                            os.listdir(basepath)
+                        except:
+                            e = 0
+                            while e < lsdir_o.__len__():
+                                f = folder_name_inner.split(' ')
+                                truth = False
+                                for op in f:
+                                    if op.upper() == folder_name_outer.upper():
+                                        continue
+                                    if lsdir_o[e].find(op):
+                                        truth = True
+
+                                if truth:
+                                    folder_name_inner = lsdir_o[e]
+                                e = e + 1
+                        basepath = './' + folder_name_outer + '/' + folder_name_inner
                         for file_name in os.listdir(basepath):
 
                             q = ''
                             k = [2014, 2015, 2016, 2017]
-                            print(q.join(re.findall("[0-9]", file_name)))
-                            if k.index(int(q.join(re.findall("[0-9]", file_name)))):
+                            print(q.join(re.findall("[0-9]", file_name)), i)
+                            try:
+
+                                k.index(int(q.join(re.findall("[0-9]", file_name))))
 
                                 if os.path.isfile(os.path.join(basepath, file_name)):
+                                    try:
+                                        k.index(int(sheet.cell_value(i, 4)))
+                                        # if sheet.row(x)[1] == folder_name_inner and sheet.row(x)[4] == q.join(
+                                        # re.findall("[0-9]", file_name)):
+                                        try:
+                                            ttt.index(i)
+                                        except:
+                                            ttt.append(i)
+                                            print(i, "ooooooo")
+                                    except:
+                                        print(i)
+
                                     src = basepath + '/' + file_name
                                     dst = './new_folder'
-
-                                    typeZ = glob.iglob(os.path.join(src, "*.zip"))
-                                    typeR = glob.iglob(os.path.join(src, "*.rar"))
-                                    for t in typeZ:
-                                        print(typeZ)
-                                        if t == 'zip':
-                                            extract = zipfile.ZipFile(src)
-                                            src = os.path.splitext(src)[0]
-                                            extract.extractall(src)
-
-                                    print('l', src, dst)
-                                    print(shutil.copy2(src, dst))
-                                    print('o')
-
-                                    print(file_name, folder_name_inner)
+                                    shutil.copy2(src, dst)
                                     s = ""
                                     folder_name_inner = folder_name_inner.replace(' ', '')
                                     folder_name_inner = folder_name_inner.replace('-', '')
 
-                                    os.rename(dst+'/'+file_name, dst+'/' + folder_name_inner + '_' + s.join(re.findall("[0-9]", file_name))+'.pdf')
+                                    os.rename(dst + '/' + file_name, dst + '/' + folder_name_inner + '_' + s.join(
+                                        re.findall("[0-9]", file_name)) + '.pdf')
+
+
+
+                            except:
+                                print('hello')
 
                     except:
                         print('jello')
@@ -106,5 +119,20 @@ for p in os.listdir('.'):
                 except:
                     print('the folder was not found A')
 
+file = 'excel.xlsx'
+wb = load_workbook(filename=file)
+ws = wb['Sheet1']
 
-        # deal with zips
+alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+# Enumerate the cells in the second row
+print(ttt)
+print(ttt.__len__())
+print(ws.max_column)
+for row in ttt:
+    col = 0
+    while col <= ws.max_column:
+        cell = ws[alpha[col] + str(row)]
+        cell.fill = PatternFill(start_color="008000", end_color="008000", fill_type="solid")
+        col = col + 1
+
+wb.save(filename=file)
