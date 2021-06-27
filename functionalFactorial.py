8caf0b160485cdb92ea9024de0579c57ff74ce33
David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>
2021-06-27 18:18:45 +0000

    updated function to utilize tail-call optimization

--- functionalFactorial.py
+++ functionalFactorial.py
@@ -1,11 +1,10 @@
 # implementing a function to find factorials in python
 
 
-def functional_factorial(value):
+def tail_call_factorial(value, num=1):
     if value == 1:
-        return value
-    else:
-        return value * functional_factorial(value - 1)
+        return num
+    return tail_call_factorial(value - 1, value * num)
 
 
-print(functional_factorial(3))
+print(tail_call_factorial(3))
